3740a14ab9dd4ad7977ae3632d063bde03d30fad
Mayur kumar <mayurkumar1111135@gmail.com>
2025-01-08 14:43:10 +0530

    now hero img is visible for small screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { ButtonOutline, ButtonPrimary } from "./Button";
 const Hero = () => {
   return (
     <section id="home" className="pt-28 lg:pt-31 ">
-      <div className="container items-center lg:grid  lg:grid-cols-2 lg:gap-10">
+      <div className="container items-center lg:grid  lg:grid-cols-2 lg:gap-5">
         <div>
           <div className="flex items-center gap-3">
             <figure className="img-box w-9 h-9 rounded-lg">
@@ -22,25 +22,12 @@ const Hero = () => {
               Available for work
             </div>
           </div>
-          <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
+          <h2 className="headline-1 max-w-[12ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
             Building Scalable Modern Websites for the Future
           </h2>
-          <div className="flex items-center gap-3">
-            <ButtonPrimary
-              label="Download CV"
-              icon="download"
-              href="/mayur-cv.pdf"
-              download={true}
-            />
-            <ButtonOutline
-              href="#about"
-              label="Scroll down"
-              icon="arrow_downward"
-            />
-          </div>
         </div>
-        <div className="hidden lg:block">
-          <figure className="w-full max-h-[517px] max-w-[376px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
+        <div className=" lg:block">
+          <figure className="max-w-[400px] max-h-[400px] lg:w-full lg:max-h-[517px] lg:max-w-[376px] ml-auto mr-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden mt-2">
             <img
               src="/images/mayur-portrait-12.png"
               width={656}
@@ -49,6 +36,19 @@ const Hero = () => {
               className="w-full"
             />
           </figure>
+        </div>
+        <div className="flex items-center justify-around mt-12 lg:justify-normal lg:gap-3 ">
+          <ButtonPrimary
+            label="Download CV"
+            icon="download"
+            href="/mayur-cv.pdf"
+            download={true}
+          />
+          <ButtonOutline
+            href="#about"
+            label="Scroll down"
+            icon="arrow_downward"
+          />
         </div>
       </div>
     </section>
